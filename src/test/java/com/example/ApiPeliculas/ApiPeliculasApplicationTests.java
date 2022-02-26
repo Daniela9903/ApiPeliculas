b4c2253ac3657b3be56a56d0990227bffd321cfc
Daniela9903 <rosas.tome@gmail.com>
2022-02-26 15:39:44 -0600

    Nuevos archivos

---
+++ src/test/java/com/example/ApiPeliculas/ApiPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiPeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
